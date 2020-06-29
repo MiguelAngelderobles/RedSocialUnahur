bf9c869e29a8ed2ae084892fa1af768078928ecd
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2020-06-28 21:15:15 -0300

    perfil sin js

---
+++ CrearPerfil.js
@@ -0,0 +1,17 @@
+function capturar(){ 
+    var nombreUsuario=document.getElementById("nombreUsuario").value
+    var contrasenia=document.getElementById("contrasenia").value
+    var email=document.getElementById("email").value
+    var Carrera=document.getElementById("Carrera").value
+    var MaterasAprobadas=document.getElementById("MaterasAprobadas").value
+    var CursandoActualmente=document.getElementById("CursandoActualmente").value
+    var Preparandofinales=document.getElementById("Preparandofinales").value
+    var onclickValue=document.getElementById("onclick").value
+    var materiaAplicada = document.getElementById("materiaAplicada").value
+    
+
+}
+
+document.getElementById("mostrar").innerHTML=
+
+    console.log(nombreUsuario,contrasenia,email,Carrera,MaterasAprobadas,CursandoActualmente,Preparandofinales,onclickValue,materiaAplicada);
