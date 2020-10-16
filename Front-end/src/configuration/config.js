13b0a78f8ab4a96abd22d5e67d4e339c1a7336ee
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2020-10-16 20:51:55 -0300

    probar

--- Front-end/src/configuration/config.js
+++ Front-end/src/configuration/config.js
@@ -1,13 +1,16 @@
-const configurApp = async (app) => {
+const configurApp =  (app) => {
     const path = require('path')
     const engine = require('ejs-mate');
     
+   
+    app.set('views',path.join(__dirname,'../src/views'))
+    app.engine('ejs', engine);
+    app.set('view engine','ejs')
+    app.engine('html', require('ejs').renderFile);
+
     app.set('port',process.env.PORT || 3000)
     app.listen(app.get('port'),()=>
     {console.log(`Server connect port ${app.get('port')}`)})
-    app.set('/src/views')
-    app.engine('ejs', engine);
-    app.set('view engine','ejs')
 }
 
 module.exports = configurApp; 
