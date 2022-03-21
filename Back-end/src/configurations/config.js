03670397d2ba397fb1541c5646d9b60e9e85413c
MiguelAngelderobles <miguel.angelderobles@otmail.com>
2022-03-21 20:36:41 -0300

    fix problem socket.io and moongose persistence

--- Back-end/src/configurations/config.js
+++ Back-end/src/configurations/config.js
@@ -1,15 +1,15 @@
 const configurApp = async (app) => {
     const http = require('http') 
-    const socketio = require('socket.io')
-    const socketUp = require('../socket/socket')
+    //const socketio = require('socket.io')
+    //const socketUp = require('../socket/socket')
 
     app.set('port',process.env.PORT || 7000)
     app.listen(app.get('port'),()=>
     {console.log(`Server connect port ${app.get('port')}`)})
 
     const server = http.createServer(app)
-    const io = socketio.listen(server)
-    socketUp(io)
+    //const io = socketio.listen(server)
+    //socketUp(io)
 
 }
 
--- Back-end/src/database/database.js
+++ Back-end/src/database/database.js
@@ -2,7 +2,7 @@ const updatabase = async()=>{
   const mongoose = require('mongoose');
   const { mongodb } = require('./keys');
 
-  mongoose.set('useFindAndModify', false);
+  //mongoose.set('useFindAndModify', false);
   mongoose.connect(mongodb.URI, {
     useNewUrlParser: true
   })
